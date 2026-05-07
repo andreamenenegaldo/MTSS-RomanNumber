3ac29ae12b2b86f18fc9bd698b4835e6eeb9acd1
Davide Menegaldo <davide.menegaldo05@gmail.com>
2026-05-07 15:59:06 +0200

    feat: implementetion roman conversion for 1-3
    closes #6

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,12 +5,41 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-    public static String print(int num) {
+
+    public static String print(final int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
 
-    private static String printAsciiArt(String romanNumber) {
-        // TODO
-        return null;
+    private static String printAsciiArt(final String romanNumber) {
+
+        switch (romanNumber) {
+
+            case "I":
+                return " _____ \n"
+                        + "|_   _|\n"
+                        + "  | |  \n"
+                        + "  | |  \n"
+                        + " _| |_ \n"
+                        + "|_____|\n";
+
+            case "II":
+                return " _____   _____ \n"
+                        + "|_   _| |_   _|\n"
+                        + "  | |     | |  \n"
+                        + "  | |     | |  \n"
+                        + " _| |_   _| |_ \n"
+                        + "|_____| |_____|\n";
+
+            case "III":
+                return " _____   _____   _____ \n"
+                        + "|_   _| |_   _| |_   _|\n"
+                        + "  | |     | |     | |  \n"
+                        + "  | |     | |     | |  \n"
+                        + " _| |_   _| |_   _| |_ \n"
+                        + "|_____| |_____| |_____|\n";
+
+            default:
+                return romanNumber;
+        }
     }
 }
