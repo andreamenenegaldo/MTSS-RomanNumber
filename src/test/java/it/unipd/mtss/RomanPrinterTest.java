bfd567386b37b5401db2e526e4f920d89fd9f84a
andreamenegaldo <andreamenegaldo04@gmail.com>
2026-05-07 15:52:00 +0200

    test: aggiunto test per i numeri 1-3

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -5,8 +5,65 @@
 package it.unipd.mtss;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanPrinterTest {
 
+    @Test
+    void print_returnsAsciiArtForOne() {
+
+        // Arrange
+        String expected =
+            " _____ \n"
+            + "|_   _|\n"
+            + "  | |  \n"
+            + "  | |  \n"
+            + " _| |_ \n"
+            + "|_____|\n";
+
+        // Act
+        String result = RomanPrinter.print(1);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void print_returnsAsciiArtForTwo() {
+
+        // Arrange
+        String expected =
+            " _____   _____ \n"
+            + "|_   _| |_   _|\n"
+            + "  | |     | |  \n"
+            + "  | |     | |  \n"
+            + " _| |_   _| |_ \n"
+            + "|_____| |_____|\n";
+
+        // Act
+        String result = RomanPrinter.print(2);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void print_returnsAsciiArtForThree() {
+
+        // Arrange
+        String expected =
+            " _____   _____   _____ \n"
+            + "|_   _| |_   _| |_   _|\n"
+            + "  | |     | |     | |  \n"
+            + "  | |     | |     | |  \n"
+            + " _| |_   _| |_   _| |_ \n"
+            + "|_____| |_____| |_____|\n";
+
+        // Act
+        String result = RomanPrinter.print(3);
+
+        // Assert
+        assertEquals(expected, result);
+    }
 }
