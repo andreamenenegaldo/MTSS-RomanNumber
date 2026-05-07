83dafb30d11fd1359524edfc791cf7701dd0c632
andreamenegaldo <andreamenegaldo04@gmail.com>
2026-05-07 18:28:09 +0200

    test: aggiunto test per i numeri 1-50

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -17,7 +17,10 @@ public class IntegerToRomanTest {
         "1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI",
         "7, VII", "8, VIII", "9, IX", "10, X", "11, XI", "12, XII",
         "13, XIII", "14, XIV", "15, XV", "16, XVI", "17, XVII", "18, XVIII",
-        "19, XIX", "20, XX"
+        "19, XIX", "20, XX", "21, XXI", "22, XXII", "23, XXIII", "24, XXIV",
+        "25, XXV", "26, XXVI", "27, XXVII", "28, XXVIII", "29, XXIX", "30, XXX",
+        "40, XL", "41, XLI", "42, XLII", "43, XLIII", "44, XLIV", "45, XLV",
+        "49, XLIX", "50, L"
     })
     void convert_returnsCorrectRomanNumeral(int input, String expected) {
         // Arrange - input e expected dal @CsvSource
